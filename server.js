391f02352a82b5998a9ffba12470a7b04e77926c
Fabiana Lima <fabianallima@hotmail.com>
2024-11-23 01:22:11 -0300

    finalização do projeto

--- server.js
+++ server.js
@@ -2,19 +2,11 @@ import express from "express";
 import routes from "./src/routes/postsRoutes.js"; 
 
 const app = express();
+app.use(express.static("uploads"))
 routes(app)
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
+
 });
 
-//function buscarPostID(id){
-//    return posts.findIndex((post) => {
-//      return post.id === Number(id)
-//  })
-//}
-
-//app.get("/posts/:id", (req, res) => {
-    //const index = buscarPostID(req.params.id)
-    //res.status(200).json(posts[index]);
-//});
